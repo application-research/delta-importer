6ae09ab4dcab33a9a82067b78498b8f0b4860015
Jason Cihelka <jcihelka@isotechnics.com>
2023-05-08 13:59:10 -0700

    feat: extract importer.go, extend GetDealStats

--- db.go
+++ db.go
@@ -21,6 +21,7 @@ const dbSchema = `
 		comm_p VARCHAR(255) NOT NULL,
 		state VARCHAR(255) NOT NULL,
 		mode VARCHAR(255) NOT NULL,
+		size BIGINT,
 		message TEXT,
 		created_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 	);
@@ -32,6 +33,7 @@ type DbImportedDeal struct {
 	CommP       string `json:"comm_p"`
 	State       string `json:"state"`
 	Mode        Mode   `json:"mode"`
+	Size        int64  `json:"size"`
 	Message     string `json:"message"`
 	CreatedDate string `json:"created_date"`
 }
@@ -59,14 +61,14 @@ func OpenDiDB(root string) (*diDB, error) {
 	}, nil
 }
 
-func (d *diDB) InsertDeal(dealUuid string, commP string, success bool, mode Mode) error {
+func (d *diDB) InsertDeal(dealUuid string, commP string, success bool, mode Mode, message string, size int64) error {
 	var state string
 	if success {
 		state = PENDING
 	} else {
 		state = FAILURE
 	}
-	_, err := d.db.Exec("INSERT INTO imported_deals (deal_uuid, comm_p, state, mode) VALUES (?, ?, ?, ?)", dealUuid, commP, state, mode)
+	_, err := d.db.Exec("INSERT INTO imported_deals (deal_uuid, comm_p, state, mode, message, size) VALUES (?, ?, ?, ?, ?, ?)", dealUuid, commP, state, mode, message, size)
 
 	if err != nil {
 		return fmt.Errorf("insert deal: %w", err)
@@ -75,14 +77,38 @@ func (d *diDB) InsertDeal(dealUuid string, commP string, success bool, mode Mode
 }
 
 type DealStats struct {
-	TotalImported int
+	TotalImported Stat
+	Pending       Stat
+	Success       Stat
+	Failed        Stat
+}
+
+type Stat struct {
+	Count uint
+	Bytes uint
 }
 
 func (d *diDB) GetDealStats() (DealStats, error) {
 	var stats DealStats
-	err := d.db.QueryRow("SELECT COUNT(*) FROM imported_deals").Scan(&stats.TotalImported)
+	err := d.db.QueryRow("SELECT COUNT(*), SUM(size) FROM imported_deals").Scan(&stats.TotalImported.Count, &stats.TotalImported.Bytes)
 	if err != nil {
-		return DealStats{}, fmt.Errorf("get deal stats: %w", err)
+		return stats, fmt.Errorf("get total deal stats: %w", err)
 	}
+
+	err = d.db.QueryRow("SELECT COUNT(*), SUM(size) FROM imported_deals WHERE state = 'PENDING'").Scan(&stats.Pending.Count, &stats.Pending.Bytes)
+	if err != nil {
+		return stats, fmt.Errorf("get pending deal stats: %w", err)
+	}
+
+	err = d.db.QueryRow("SELECT COUNT(*), SUM(size) FROM imported_deals WHERE state = 'SUCCESS'").Scan(&stats.Success.Count, &stats.Success.Bytes)
+	if err != nil {
+		return stats, fmt.Errorf("get success deal stats: %w", err)
+	}
+
+	err = d.db.QueryRow("SELECT COUNT(*), SUM(size) FROM imported_deals WHERE state = 'FAILED'").Scan(&stats.Failed.Count, &stats.Failed.Bytes)
+	if err != nil {
+		return stats, fmt.Errorf("get failed deal stats: %w", err)
+	}
+
 	return stats, nil
 }
---
+++ importer.go
@@ -0,0 +1,236 @@
+package main
+
+import (
+	"context"
+	"time"
+
+	"github.com/google/uuid"
+	log "github.com/sirupsen/logrus"
+)
+
+func importer(cfg Config, db *diDB, datasets map[string]Dataset) {
+	// We construct a new Boost connection at each run of the importer, as this is resilient in case boost is down/restarts
+	// It will simply re-connect upon the next run of the importer
+	boost, err := NewBoostConnection(cfg.BoostAddress, cfg.BoostPort, cfg.BoostGqlPort, cfg.BoostAPIKey)
+	if err != nil {
+		log.Errorf("error creating boost connection: %s", err.Error())
+		return
+	}
+	defer boost.close()
+
+	inProgress := boost.GetDealsInPipeline()
+
+	if cfg.MaxConcurrent != 0 && len(inProgress) >= int(cfg.MaxConcurrent) {
+		log.Infof("skipping import job as there are already %d deals in progress (max_concurrent is %d)", len(inProgress), cfg.MaxConcurrent)
+		return
+	}
+
+	log.Debugf("found %d deals in sealing pipeline", len(inProgress))
+
+	var importResult *ImportResult
+
+	// Attempt to import a deal for each dataset in order - if any dataset fails, go to the next one
+	for _, ds := range datasets {
+		log.Debugf("searching for a deal for dataset %s", ds.Dataset)
+
+		switch cfg.Mode {
+		case ModePullDataset:
+			importResult = importerPullDataset(cfg, ds, boost)
+		case ModePullCID:
+			importResult = importerPullCid(cfg, ds, boost)
+		default:
+			importResult = importerDefault(cfg, ds, boost)
+		}
+
+		if importResult != nil {
+			db.InsertDeal(importResult.DealUuid, importResult.CommP, importResult.Successful, cfg.Mode, importResult.Message, importResult.FileSize)
+		}
+
+		if importResult != nil && importResult.Successful {
+			break
+		}
+	}
+}
+
+var cidsAlreadyAttempted = make(map[string]bool)
+
+func importerDefault(cfg Config, ds Dataset, boost *BoostConnection) *ImportResult {
+	toImport := boost.GetDealsAwaitingImport(ds.Address)
+
+	if len(toImport) == 0 {
+		log.Debugf("skipping dataset %s : no deals awaiting import", ds.Dataset)
+		return nil
+	}
+
+	log.Debugf("%d deals awaiting import for dataset %s", len(toImport), ds.Dataset)
+
+	// Start with the last (oldest) deal
+	i := len(toImport)
+	// keep trying until we successfully manage to import a deal
+	// this should usually simply take the first one, import it, and then return
+	for i > 0 {
+		i = i - 1
+		deal := toImport[i]
+
+		// Don't attempt more than once
+		if cidsAlreadyAttempted[deal.PieceCid] {
+			continue
+		}
+		cidsAlreadyAttempted[deal.PieceCid] = true
+
+		if deal.StartEpoch.IntoUnix() < time.Now().Add(MIN_SEALING_TIME).Unix() {
+			log.Debugf("skipping deal %s as it would be past the start epoch when sealing completes", deal.ID)
+			continue
+		}
+
+		// See if we have failed this CID before with mismatched commP
+		otherDeals := boost.GetDealsForContent(deal.PieceCid)
+		if HasMismatchedCommPErrors(otherDeals) {
+			log.Debugf("skipping import of %s as there are mismatched CommP errors for it", deal.PieceCid)
+			continue
+		}
+
+		filename := ds.GenerateCarFileName(deal.PieceCid)
+		if filename == "" {
+			log.Errorf("could not find carfile name for dataset %s for CID %s", ds.Dataset, deal.PieceCid)
+			continue
+		}
+
+		if !FileExists(filename) {
+			log.Errorf("could not find carfile %s for dataset %s for CID %s", filename, ds.Dataset, deal.PieceCid)
+			continue
+		}
+
+		id, err := uuid.Parse(deal.ID)
+		if err != nil {
+			log.Errorf("could not parse uuid " + deal.ID)
+			continue
+		}
+
+		importResult := boost.ImportCar(context.Background(), filename, deal.PieceCid, id)
+		return &importResult
+	}
+
+	log.Infof("attempted all deals for for dataset %s, none could be imported", ds.Dataset)
+	return nil
+}
+
+func importerPullDataset(cfg Config, ds Dataset, boost *BoostConnection) *ImportResult {
+	ddm := NewDDMApi(cfg.DDMURL, cfg.DDMToken)
+
+	log.Infof("requesting deal for dataset %s", ds.Dataset)
+	pieceCid, err := ddm.RequestDealForDataset(ds.Dataset)
+	if err != nil {
+		log.Errorf("error requesting deal for dataset %s: %s", ds.Dataset, err.Error())
+		return nil
+	}
+	if pieceCid == "" {
+		log.Errorf("no deal returned for dataset %s", ds.Dataset)
+		return nil
+	}
+
+	// Successfully requested a deal - wait for it to show up in Boost
+	readyToImport, err := boost.WaitForDeal(pieceCid)
+	if err != nil {
+		log.Errorf("error waiting for deal for dataset %s: %s", ds.Dataset, err.Error())
+		return nil
+	}
+
+	// * Note: We don't need to check HasMismatchedCommPErrors here, as this should result in a newly requested deal. DDM should not allow multiple re-deals if it had been previously dealt
+
+	// Deal has been made with boost - import it
+	// There may be several deals matching the pieceCid, but we only want to import one - take the first one
+	deal := readyToImport[0]
+	filename := ds.GenerateCarFileName(pieceCid)
+
+	if !FileExists(filename) {
+		log.Debugf("could not find carfile %s for dataset %s for CID %s", filename, ds.Dataset, pieceCid)
+		return nil
+	}
+
+	id, err := uuid.Parse(deal.ID)
+	if err != nil {
+		log.Errorf("could not parse uuid " + deal.ID)
+		return nil
+	}
+
+	importResult := boost.ImportCar(context.Background(), filename, pieceCid, id)
+	return &importResult
+}
+
+func importerPullCid(cfg Config, ds Dataset, boost *BoostConnection) *ImportResult {
+	ddm := NewDDMApi(cfg.DDMURL, cfg.DDMToken)
+	carFilePaths := ds.CarFilePaths()
+
+	ds.PopulateAlreadyImportedCids(boost)
+
+	if len(carFilePaths) == 0 {
+		log.Debugf("skipping dataset %s : no car files found", ds.Dataset)
+		return nil
+	}
+
+	log.Debugf("%d car files found for dataset %s", len(carFilePaths), ds.Dataset)
+
+	for _, carFilePath := range carFilePaths {
+		// Assume files are named as <cidFromFilename>.car
+		cidFromFilename := FileNameFromPath(carFilePath)
+
+		if ds.IsCidAlreadyImported(cidFromFilename) {
+			log.Debugf("skipping import of %s as it's already been imported previously", cidFromFilename)
+			continue
+		}
+
+		// Don't attempt any given carfile import more than once
+		if cidsAlreadyAttempted[cidFromFilename] {
+			continue
+		}
+		cidsAlreadyAttempted[cidFromFilename] = true
+
+		// See if we have failed this CID before with mismatched commP
+		otherDeals := boost.GetDealsForContent(cidFromFilename)
+		if HasMismatchedCommPErrors(otherDeals) {
+			log.Debugf("skipping import of %s as there are mismatched CommP errors for it", cidFromFilename)
+			continue
+		}
+
+		log.Infof("requesting deal for dataset %s, cid %s", ds.Dataset, cidFromFilename)
+		pieceCid, err := ddm.RequestDealForCid(cidFromFilename)
+		if err != nil {
+			log.Errorf("error requesting deal for cid %s: %s", cidFromFilename, err.Error())
+			return nil
+		}
+		if pieceCid == "" {
+			log.Errorf("no deal returned for dataset %s", ds.Dataset)
+			return nil
+		}
+
+		// Successfully requested a deal - wait for it to show up in Boost
+		readyToImport, err := boost.WaitForDeal(pieceCid)
+		if err != nil {
+			log.Errorf("error waiting for deal for dataset %s: %s", ds.Dataset, err.Error())
+			return nil
+		}
+
+		// Deal has been made with boost - import it
+		// There may be several deals matching the pieceCid, but we only want to import one - take the first one
+		deal := readyToImport[0]
+
+		// This should not happen as we just read the file, but check anyway in case the file has been deleted very recently
+		if !FileExists(carFilePath) {
+			log.Errorf("could not find carfile %s for dataset %s for CID %s. it must have been deleted", carFilePath, ds.Dataset, pieceCid)
+			return nil
+		}
+
+		id, err := uuid.Parse(deal.ID)
+		if err != nil {
+			log.Errorf("could not parse uuid " + deal.ID)
+			return nil
+		}
+
+		importResult := boost.ImportCar(context.Background(), carFilePath, pieceCid, id)
+		return &importResult
+	}
+
+	log.Infof("attempted to import all carfiles for dataset %d, but none could be imported", ds.Dataset)
+	return nil
+}
