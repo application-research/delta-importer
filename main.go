57d100565af562c792305a658366bdc01b3fdec3
Jason Cihelka <jcihelka@isotechnics.com>
2023-05-08 15:59:55 -0700

    feat: cleanup

--- main.go
+++ main.go
@@ -14,14 +14,6 @@ import (
 var Commit string
 var Version string
 
-var Reset = "\033[0m"
-var Purple = "\033[35m"
-var Cyan = "\033[36m"
-var Gray = "\033[37m"
-var White = "\033[97m"
-var Red = "\033[31m"
-var Green = "\033[32m"
-
 func main() {
 
 	app := &cli.App{
--- util.go
+++ util.go
@@ -8,6 +8,14 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
+var Reset = "\033[0m"
+var Purple = "\033[35m"
+var Cyan = "\033[36m"
+var Gray = "\033[37m"
+var White = "\033[97m"
+var Red = "\033[31m"
+var Green = "\033[32m"
+
 func FileExists(path string) bool {
 	_, err := os.Stat(path)
 	if err != nil {
