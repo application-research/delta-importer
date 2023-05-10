c458ddf14658015746511e417e7c12e991a4b803
Jason Cihelka <jcihelka@isotechnics.com>
2023-05-09 17:22:09 -0700

    feat: fix import

--- daemon/api/health.go
+++ daemon/api/health.go
@@ -1,7 +1,7 @@
 package api
 
 import (
-	"github.com/labstack/echo"
+	"github.com/labstack/echo/v4"
 )
 
 // Health check routes for verifying API is alive
--- daemon/api/stats.go
+++ daemon/api/stats.go
@@ -2,7 +2,7 @@ package api
 
 import (
 	"github.com/application-research/delta-importer/db"
-	"github.com/labstack/echo"
+	"github.com/labstack/echo/v4"
 )
 
 func ConfigureStatsRouter(e *echo.Group, db *db.DIDB) {
